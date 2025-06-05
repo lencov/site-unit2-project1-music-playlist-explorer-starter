2649a80ff7bc89a84fd2e191e47255458e81e977
Lenox 'A'ali'i Covington <lenox@meta.com>
2025-06-04 23:02:59 -0700

    create playlist cards and modal

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,20 @@
+const playlists = [
+  {
+    name: "Chill Beats",
+    imageUrl: "assets/img/playlist.png",
+    author: "LoFi Girl",
+    songs: [
+      { title: "Rainy Nights", artist: "Nujabes", duration: "3:20" },
+      { title: "Ocean Breeze", artist: "Idealism", duration: "2:45" }
+    ]
+  },
+  {
+    name: "Workout Pump",
+    imageUrl: "assets/img/pump.png",
+    author: "Beast Mode",
+    songs: [
+      { title: "Push It", artist: "Salt-N-Pepa", duration: "3:30" },
+      { title: "Eye of the Tiger", artist: "Survivor", duration: "4:05" }
+    ]
+  }
+];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,58 @@
 
+const span = document.getElementsByClassName("close")[0];
+
+function openModal(playlist) {
+   console.log('openModal is being called');
+   console.log(playlist)
+   document.getElementById('modalTitle').innerText = playlist.name;
+   document.getElementById('modalImage').src = playlist.imageUrl;
+   document.getElementById('modalAuthor').innerText = `By: ${playlist.author}`;
+   
+   const songList = document.getElementById('modalSongList');
+   songList.innerHTML = "";
+   playlist.songs.forEach(song => {
+    const li = document.createElement('li');
+    
+    const titleSpan = document.createElement("span");
+    titleSpan.className = "song-title";
+    titleSpan.textContent = song.title;
+
+    const artistSpan = document.createElement("span");
+    artistSpan.className = "song-artist";
+    artistSpan.textContent = ` - ${song.artist}`;
+
+    const durationSpan = document.createElement("span");
+    durationSpan.className = "song-duration";
+    durationSpan.textContent = ` (${song.duration})`;
+
+    li.append(titleSpan);
+    li.append(artistSpan);
+    li.append(durationSpan);
+
+    songList.append(li);
+   });
+
+   document.getElementById("playlistModal").style.display = "block";
+}
+
+// close modal
+span.onclick = function() {
+   document.getElementById("playlistModal").style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == document.getElementById("playlistModal")) {
+      document.getElementById("playlistModal").style.display = "none";
+   }
+}
+
+// create event listener for all playlist cards
+const cards = document.querySelectorAll('.card');
+cards.forEach( card => {
+    const index = parseInt(card.dataset.playlistIndex);
+    card.addEventListener('click', () => {
+        console.log(`opening modal with playlist at index ${index}`)
+        openModal(playlists[index]);
+    })
+
+});
+
